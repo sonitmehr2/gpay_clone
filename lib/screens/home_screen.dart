8399f6669de0123f94dd8c26c0e6335958e64ca6
User <sonitmyl@gmal.com>
2023-12-06 00:52:07 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+              onPressed: () async {
+                await checkFirebase();
+              },
+              child: Text('Firebase check'))
+        ]),
+      ),
+    );
+  }
+
+  Future<void> checkFirebase() async {}
+}
